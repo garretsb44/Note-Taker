ad36e86e1aee7cf50e01e56300f4068f875700eb
Garret Bright <garret@drinknewtopia.com>
2020-10-08 19:19:16 -0700

    added html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+// Similar to the apiRoute.js module, this module also exports a function which
+// accepts an Express app object and sets up routes. This function adds routes
+// for serving the html files.
+module.exports = function (app) {
+  // Below code handles when users navigate to a page. (e.g. follow a link or
+  // enter a url in the address bar) The client receives an HTML document to
+  // show the user as a response for each of these routes.
+  app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "../develop/public/index.html"));
+  });
+
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../develop/public/notes.html"));
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// The code below points the server to a series of "route" files. These routes
+// give our server a "map" of how to respond when users visit or request data
+// from various URLs.
+require("./routes/index")(app);
+
+
+// The below code effectively "starts" our server
+app.listen(PORT, () => {
+  console.log("App listening on PORT: " + PORT);
+});

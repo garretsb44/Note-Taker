abb170e53be8e5e54a87f1453840529958338efd
Garret Bright <garret@drinknewtopia.com>
2020-10-29 13:27:36 -0700

    added npm compression

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,12 @@ let db = require("../db/db.json");
 const fs = require("fs");
 const shortid = require('shortid');
 const path = require("path");
+var compression = require('compression')
 
 // This module exports a function which accepts an Express app object and
 // and sets up the api routes.
 module.exports = function (app) {
-    //
+    app.use(compression())
     // Below code handles when users "visit" a page. In each of the below cases
     // when a user visits a link (ex: localhost:PORT/api/admin... they are shown a
     // JSON of the data in the table)
